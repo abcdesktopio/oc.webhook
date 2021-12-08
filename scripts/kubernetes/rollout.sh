1bfbc18f32418c53b552fce00883b9ab0ccb920b
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2021-12-08 14:13:33 +0100

    change daemonset param

--- scripts/kubernetes/rollout.sh
+++ scripts/kubernetes/rollout.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+if [ -z $daemonset ]; then
+	daemonset=$1
+fi
+
+if [ -z $daemonset ]; then
+	echo 'daemonset not set use all'
+	daemonset=""
+fi 
+
 kubectl rollout restart daemonset $daemonset -n $NAMESPACE
